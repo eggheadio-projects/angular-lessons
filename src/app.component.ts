a47482ac0e439cf6ee2d73b8327757d44403f199
John Lindquist <johnlindquist@gmail.com>
2016-11-14 16:23:44 -0700

    ngTemplateOutlet

--- src/app.component.ts
+++ src/app.component.ts
@@ -3,9 +3,30 @@ import { Component } from '@angular/core'
 @Component({
     selector: 'app',
     template: `
-        <h1>{{message}}</h1>
+<template #foo let-whatever="message">
+<h1>{{whatever}}</h1>
+</template>
+
+<div 
+    [ngTemplateOutlet]="foo"
+    [ngOutletContext]="one"
+    >    
+</div>
+<div 
+    [ngTemplateOutlet]="foo"
+    [ngOutletContext]="two"
+    >    
+</div>
+<div 
+    [ngTemplateOutlet]="foo"
+    [ngOutletContext]="three"
+    >    
+</div>
     `
 })
 export class AppComponent {
-    message = `Hello, Angular`
+    one = {message:'Hello One'}
+    two = {message:'Hello Two'}
+    three = {message:'Hello Three'}
+
 }
