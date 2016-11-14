5519bd283ad40e7b33fa8b0a3cfc35125c63a80c
John Lindquist <johnlindquist@gmail.com>
2016-11-14 15:56:11 -0700

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { AppComponent } from './app.component'
+
+const imports = [BrowserModule]
+const bootstrap = [AppComponent]
+const declarations = [
+    AppComponent
+]
+
+@NgModule({
+    imports,
+    declarations,
+    bootstrap
+})
+export class AppModule {}
